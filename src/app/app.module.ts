a986ea5dfb939e810d333eae7510d6e61de7bca6
Israel Bermeo <israel.bermeo@gmail.com>
2018-04-05 20:44:35 -0500

    Creado el componente pruebasri

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PruebasriComponent } from './pruebasri/pruebasri.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PruebasriComponent
   ],
   imports: [
     BrowserModule
